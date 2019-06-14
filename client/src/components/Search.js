8dd96cd5cce636424fff526919df1602e8559da2
Kristina Taing <kataing12@gmail.com>
2019-06-13 21:54:40 -0700

    Completed Steps 1, 2, 3, 4

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -4,16 +4,29 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      value: ''
     }
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.value);
   }
   
   render () {
     return(
       <div>
         <form action="" >
-          <input type="text" />
-          <button></button>
+          <input onChange={this.handleInput} type="text" />
+          <button onClick={this.handleSubmit}>button</button>
         </form>
       </div>
     );
